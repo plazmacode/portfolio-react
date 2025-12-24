1e6899d965e89845a6c9b8bb04bc1a77afdc959d
Tobias <tobiaskrogshede@gmail.com>
2025-12-24 19:54:57 +0100

    feat: jank WIP text over GameBox link

--- src/components/SelectedWorks/SelectedWorks.tsx
+++ src/components/SelectedWorks/SelectedWorks.tsx
@@ -18,8 +18,13 @@ function SelectedWorks() {
           <img src={FloraHiveMetrics} className="image-card" style={{ width: '320px' }}></img>
         </Link>
       </div>
-      <div>
-        <img src={GameBox} className="image-card" style={{ width: '320px' }}></img>
+      <div className="position-relative d-inline-block">
+        <Link to="#">
+          <img src={GameBox} className="image-card" style={{ width: '320px' }}></img>
+          <div style={{ zIndex: 1 }}>
+            <p className="position-absolute top-50 start-50 translate-middle fw-bold text-white bg-dark bg-opacity-50 p-1 rounded fs-1">WIP</p>
+          </div>
+        </Link>
       </div>
     </div>
     </>
