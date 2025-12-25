23227d6d3c4810c2921a08e589099b5561748a89
Tobias <tobiaskrogshede@gmail.com>
2025-12-25 16:28:08 +0100

    feat: better letter scrambling

--- src/components/Infobox/Infobox.tsx
+++ src/components/Infobox/Infobox.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, type ReactNode } from 'react';
 import './Infobox.css';
 
 interface InfoboxProps {
@@ -7,55 +7,56 @@ interface InfoboxProps {
 }
 
 function Infobox({ topText, bottomText }: InfoboxProps) {
-  const chars = "0123456789ABCDEF";
+  const chars = "*01{}/\\";
 
-  const [displayTop, setDisplayTop] = useState(topText);
-  const [displayBottom, setDisplayBottom] = useState(bottomText);
+  const [displayTop, setDisplayTop] = useState<ReactNode[]>([]);
+  const [displayBottom, setDisplayBottom] = useState<ReactNode[]>([]);
 
   const topIntervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
   const bottomIntervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
 
   useEffect(() => {
     scrambleText(topText, setDisplayTop, topIntervalRef);
+    const currentTopRef = topIntervalRef.current;
     return () => {
-        if (topIntervalRef.current) clearInterval(topIntervalRef.current);
-    };
+      if (currentTopRef) clearInterval(currentTopRef);    };
   }, [topText]);
 
   useEffect(() => {
     scrambleText(bottomText, setDisplayBottom, bottomIntervalRef);
+    const currentBottomRef = bottomIntervalRef.current;
     return () => {
-        if (bottomIntervalRef.current) clearInterval(bottomIntervalRef.current);
+        if (currentBottomRef) clearInterval(currentBottomRef);
     };
   }, [bottomText]);
 
   function scrambleText(
     target: string, 
-    setter: (val: string) => void, 
-    ref: React.MutableRefObject<ReturnType<typeof setInterval> | null>
-  ) {
+    setter: (val: ReactNode[]) => void, 
+    ref: React.RefObject<ReturnType<typeof setInterval> | null>  ) {
     if (ref.current) clearInterval(ref.current);
 
     let iteration = 0;
     ref.current = setInterval(() => {
-      setter(
-        target
-          .split("")
-          .map((_, index) => {
-            if (index < iteration) {
-              return target[index];
-            }
-            return chars[Math.floor(Math.random() * chars.length)];
-          })
-          .join("")
-      );
+      const scrambled = target.split("").map((letter, index) => {
+        if (index < iteration) { // Finished letters
+          return <span key={index}>{target[index]}</span>;
+        }
+        return ( // transparent scrambling letters
+          <span key={index} className="scrambling">
+            {chars[Math.floor(Math.random() * chars.length)]}
+          </span>
+        );
+      });
+
+      setter(scrambled);
 
       if (iteration >= target.length) {
         if (ref.current) clearInterval(ref.current);
       }
 
-      iteration += 1; 
-    }, 30);
+      iteration += 0.5;
+    }, 5);
   }
 
   return (
