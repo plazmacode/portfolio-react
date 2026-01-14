a6255e80ae26748bd2323930072c9589930945c1
Tobias <tobiaskrogshede@gmail.com>
2026-01-14 11:41:49 +0100

    style: more readable

--- src/pages/FloraHiveMetrics/MetricsPreview2.tsx
+++ src/pages/FloraHiveMetrics/MetricsPreview2.tsx
@@ -282,8 +282,8 @@ export async function SimulateRun(settings: RunSimulationSettings, count: number
 }
 
 export async function GetAllRuns(token: string, userRuns: boolean = true): Promise<Run[]> {
-  let endpoint = `${import.meta.env.VITE_METRICS_BACKEND}/sim/run/`;
-  if (!userRuns) { endpoint += "all"; }
+  let endpoint = `${import.meta.env.VITE_METRICS_BACKEND}/sim/run`;
+  if (!userRuns) { endpoint += "/all"; }
 
   const getResponse = await fetch(endpoint, {
     method: "GET",
